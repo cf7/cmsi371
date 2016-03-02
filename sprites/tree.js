18183604429970fa0a9c512517ea56d50db8528d
cf7 <cfranco7@lion.lmu.edu>
2016-03-02 02:05:53 -0800

    Resubmit Edits (start) - externalizing translate

--- sprites/tree.js
+++ sprites/tree.js
@@ -115,7 +115,7 @@ $(function () {
         var growTree = function (tree) {
             // clear();
             ctx.save();
-            ctx.translate(tree.trunk.position.x, tree.trunk.position.y);
+            //ctx.translate(tree.trunk.position.x, tree.trunk.position.y);
             ctx.fillStyle = tree.barkColor;
             trunk(tree.trunk);
             branches(tree.branches);
